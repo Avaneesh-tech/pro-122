fb3d6d8c8d9a707699b534fa55e5b08610b0da6d
Avaneesh-tech <70690266+Avaneesh-tech@users.noreply.github.com>
2022-02-24 15:13:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sklearn.datasets import fetch_openml
+
+#Fetching the data
+X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+print(pd.Series(y).value_counts())
+classes = ['0', '1', '2','3', '4','5', '6', '7', '8', '9']
+nclasses = len(classes)
